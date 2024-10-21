e6920768ac6edb54c0038c98b4c017dbe1928564
Anas <anasashraf1170@gmail.com>
2024-10-21 04:19:55 +0300

    last update

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -9,9 +9,6 @@ module "eks" {
   subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
-  cluster_upgrade_policy = {
-    type = "Standard"
-  }
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
